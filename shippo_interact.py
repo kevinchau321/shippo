f4b9e8839228ebe0ce6910258575f0a161a6a265
kevinchau321 <kevinchau321@gmail.com>
2016-11-09 21:41:47 -0800

    started working on pdf to csv parser

--- shippo_interact.py
+++ shippo_interact.py
@@ -6,23 +6,70 @@ import tempfile
 import urllib
 import datetime
 
-def download_pdf(lnk):
-    options = webdriver.ChromeOptions()
-    tgt = tempfile.mkdtemp()
-    profile = {"plugins.plugins_list": [{"enabled":False,"name":"Chrome PDF Viewer"}],
-        "download.default_directory" : tgt}
-    options.add_experimental_option("prefs",profile)
-    driver = webdriver.Chrome(CHROMEDRIVER, chrome_options = options)
-    driver.get(lnk)
-    driver.find_element_by_id('userName1').send_keys('username')
-    driver.find_element_by_id('password1').send_keys('password')
-    driver.find_element_by_id('loginButton1').click()
+def pdf_to_csv(filename, separator, threshold):
+    from cStringIO import StringIO
+    from pdfminer.converter import LTChar, TextConverter
+    from pdfminer.layout import LAParams
+    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+    from pdfminer.pdfpage import PDFPage
 
-    ftgt = os.path.join(tgt,'downloaed.pdf')
-    while not os.path.exists(ftgt):
-        time.sleep(3)
-    driver.close()
-    return ftgt
+    class CsvConverter(TextConverter):
+        def __init__(self, *args, **kwargs):
+            TextConverter.__init__(self, *args, **kwargs)
+            self.separator = separator
+            self.threshold = threshold
+
+        def end_page(self, i):
+            from collections import defaultdict
+            lines = defaultdict(lambda: {})
+            for child in self.cur_item._objs:  # <-- changed
+                if isinstance(child, LTChar):
+                    (_, _, x, y) = child.bbox
+                    line = lines[int(-y)]
+                    line[x] = child._text.encode(self.codec)  # <-- changed
+            for y in sorted(lines.keys()):
+                line = lines[y]
+                self.line_creator(line)
+                self.outfp.write(self.line_creator(line))
+                self.outfp.write("\n")
+
+        def line_creator(self, line):
+            keys = sorted(line.keys())
+            # calculate the average distange between each character on this row
+            average_distance = sum([keys[i] - keys[i - 1] for i in range(1, len(keys))]) / len(keys)
+            # append the first character to the result
+            result = [line[keys[0]]]
+            for i in range(1, len(keys)):
+                # if the distance between this character and the last character is greater than the average*threshold
+                if (keys[i] - keys[i - 1]) > average_distance * self.threshold:
+                    # append the separator into that position
+                    result.append(self.separator)
+                # append the character
+                result.append(line[keys[i]])
+            printable_line = ''.join(result)
+            return printable_line
+
+    # ... the following part of the code is a remix of the
+    # convert() function in the pdfminer/tools/pdf2text module
+    rsrc = PDFResourceManager()
+    outfp = StringIO()
+    device = CsvConverter(rsrc, outfp, codec="utf-8", laparams=LAParams())
+    # becuase my test documents are utf-8 (note: utf-8 is the default codec)
+
+    fp = open(filename, 'rb')
+
+    interpreter = PDFPageInterpreter(rsrc, device)
+    for i, page in enumerate(PDFPage.get_pages(fp)):
+        outfp.write("START PAGE %d\n" % i)
+        if page is not None:
+            # print 'none'
+            interpreter.process_page(page)
+        outfp.write("END PAGE %d\n" % i)
+
+    device.close()
+    fp.close()
+
+    return outfp.getvalue()
 
 with Browser('chrome') as browser:
     #browser = Browser('chrome')
@@ -90,6 +137,8 @@ with Browser('chrome') as browser:
     browser.windows.current = browser.windows[0]
     browser.windows[1].close()
     # Parse Packing slips for addresses
+
+    print pdf_to_csv(pdf_filename, "\n", 1.5)
 
     # Call shippo API to create shipment labels and transactions
 
